d6d321a1d17429a34e13c4734861c79f5e428906
toseinaxy <ungarl40@gmail.com>
2026-09-07 22:03:34 +0200

    Update Jarvis.js

--- Jarvis.js
+++ Jarvis.js
@@ -1,9 +1,11 @@
+const say = require("say");
 const config = require("./Config.js");
 
 console.log(config.name + " wurde gestartet!");
 
 function jarvis(text) {
   console.log(config.name + ":", text);
+  say.speak(text);
 }
 
-jarvis("Hallo! Ich bin " + config.name + ".");
+jarvis("Hallo! Ich bin Jarvis.");
