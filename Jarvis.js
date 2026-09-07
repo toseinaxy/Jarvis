c12678c2af04b40bc5323ad036b9b187f3f8e95a
toseinaxy <ungarl40@gmail.com>
2026-09-07 21:25:05 +0200

    Update Jarvis.js

--- Jarvis.js
+++ Jarvis.js
@@ -1,3 +1,7 @@
-console. log("Jarvis wurde gestartet !");
+console.log("Jarvis wurde gestartet!");
+
 function jarvis(text) {
-console. log("Jarvis:", text); { jarvis("Hallo! Ich bin Jarvis.");
+  console.log("Jarvis:", text);
+}
+
+jarvis("Hallo! Ich bin Jarvis.");
